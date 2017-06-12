aa1b4e2337845d1a368315400056c955118afc2d
大祭 <lidong.cld@alibaba-inc.com>
2017-06-12 11:11:19 +0800

    添加工程文件

---
+++ WCDB/WCDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCDB
+//
+//  Created by caolidong on 2017/6/12.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCDB/WCDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WCDB
+//
+//  Created by caolidong on 2017/6/12.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
